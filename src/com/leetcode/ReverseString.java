4afab31ee3ca823379572f92a1f160fc39d7f2fc
hezhigang <hezhigangthink@gmail.com>
2018-08-22 07:27:34 -0400

    一题多解

--- src/com/leetcode/ReverseString.java
+++ src/com/leetcode/ReverseString.java
@@ -9,21 +9,34 @@ package com.leetcode;
  * 344. Reverse String
  */
 public class ReverseString {
-    public String reverseString(String s) {
+	
+    public static String reverseString(String s) {
         StringBuilder sb = new StringBuilder();
         if (s!=null && !s.isEmpty()) {
-            for(int i=s.length()-1;i>=0;i--) {
-                sb.append(s.charAt(i));
+        	char[] arr = s.toCharArray();
+            for(int i=arr.length-1;i>=0;i--) {
+                sb.append(arr[i]);
             }
         }
         return sb.toString();
-    }	
+    }
+    
+    public static String reverseString2(String s) {
+        String r = "";
+        if (s!=null && !s.isEmpty()) {
+        	StringBuilder sb = new StringBuilder(s);
+        	r = sb.reverse().toString();
+        }
+        return r;
+    }    
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-
+		String s = "A man, a plan, a canal: Panama";
+		System.out.println(s);
+		System.out.println( reverseString2(s) );
 	}
 
 }
