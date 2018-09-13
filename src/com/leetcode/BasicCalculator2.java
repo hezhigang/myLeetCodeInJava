8bbe3a4b570d2699080f71e151576c342549e53c
hezhigang <hezhigangthink@gmail.com>
2018-09-13 19:10:22 -0400

    227. Basic Calculator II
    leetcode test cases pass

--- src/com/leetcode/BasicCalculator2.java
+++ src/com/leetcode/BasicCalculator2.java
@@ -5,6 +5,8 @@
  */
 package com.leetcode;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Stack;
 import java.util.StringTokenizer;
 import java.util.regex.Matcher;
@@ -21,9 +23,10 @@ public class BasicCalculator2 {
 	 * @return
 	 */
     public static int calculate(String s) {
-    	System.out.println(s);
-    	String expr = infix2postfix(s.replaceAll(" ", ""));
-    	System.out.println(expr);
+//    	System.out.println(s);
+    	List<String> expr = infix2postfix(s.replaceAll(" ", ""));
+//    	for(int i=0;i<expr.size();i++)
+//    		System.out.println(expr.get(i));
         return postfixCalculator(expr);
     }
     
@@ -32,41 +35,41 @@ public class BasicCalculator2 {
 	 * @param postfixExpr
 	 * @return
 	 */
-	public static int postfixCalculator(String postfixExpr) {
-		Stack stk = new Stack();
-		char[] arr = postfixExpr.toCharArray();
-		for (int i = 0; i < arr.length; i++) {
-			char c = arr[i];
+	public static int postfixCalculator(List<String> postfixExpr) {
+		Stack<Integer> stk = new Stack<Integer>();
+//		char[] arr = postfixExpr.toCharArray();
+		for (int i = 0; i < postfixExpr.size(); i++) {
+			String s = postfixExpr.get(i);
 			int a, b, x;
-			switch (c) {
-			case '*':
+			switch (s) {
+			case "*":
 				if (!stk.isEmpty()) {
-					a = ((Integer) stk.pop()).intValue();
-					b = ((Integer) stk.pop()).intValue();
+					a = stk.pop().intValue();
+					b = stk.pop().intValue();
 					x = a * b;
 					stk.push(new Integer(x));
 				}
 				break;
-			case '+':
+			case "+":
 				if (!stk.isEmpty()) {
-					a = ((Integer) stk.pop()).intValue();
-					b = ((Integer) stk.pop()).intValue();
+					a = stk.pop().intValue();
+					b = stk.pop().intValue();
 					x = a + b;
 					stk.push(new Integer(x));
 				}
 				break;
-			case '-':
+			case "-":
 				if (!stk.isEmpty()) {
-					a = ((Integer) stk.pop()).intValue();
-					b = ((Integer) stk.pop()).intValue();
+					a = stk.pop().intValue();
+					b = stk.pop().intValue();
 					x = b - a;
 					stk.push(new Integer(x));
 				}
 				break;
-			case '/':
+			case "/":
 				if (!stk.isEmpty()) {
-					a = ((Integer) stk.pop()).intValue();
-					b = ((Integer) stk.pop()).intValue();
+					a = stk.pop().intValue();
+					b = stk.pop().intValue();
 					x = b / a;
 					stk.push(new Integer(x));
 				}
@@ -82,13 +85,13 @@ public class BasicCalculator2 {
 //			case '8':
 //			case '9':
 			default: 
-				stk.push(new Integer(Character.digit(c, 10)));
+				stk.push(new Integer(s));
 				break;
 			}
 		}
 		int ret = 0;
 		if (!stk.isEmpty())
-			ret = ((Integer) stk.pop()).intValue();
+			ret = stk.pop().intValue();
 		return ret;
 	}
 	
@@ -97,58 +100,67 @@ public class BasicCalculator2 {
 	 * @param infixExpr
 	 * @return
 	 */
-	public static String infix2postfix(String infixExpr) {
-		StringBuffer postfixExpr = new StringBuffer();
-		Stack stk = new Stack();
-		char[] arr = infixExpr.toCharArray();
-		for (int i = 0; i < arr.length; i++) {
-			char c = arr[i];
-			switch (c) {
-			case '*':
-			case '/':
-				while (!stk.isEmpty() && (((Character) stk.peek()).charValue() == '*' || ((Character) stk.peek()).charValue() == '/')) {
-					postfixExpr.append(((Character) stk.pop()).charValue());
+	public static List<String> infix2postfix(String infixExpr) {
+		List<String> r = new ArrayList<String>();
+//		StringBuffer postfixExpr = new StringBuffer();
+		Stack<String> stk = new Stack<String>();
+//		char[] arr = infixExpr.toCharArray();
+		List<String> list = new ArrayList<String>();
+		StringTokenizer st = new StringTokenizer(infixExpr,"[\\+\\-\\*/]",true);
+	     while (st.hasMoreTokens()) {
+	         list.add(st.nextToken());
+	     }
+		for (int i = 0; i < list.size(); i++) {
+			String s = list.get(i);
+			switch (s) {
+			case "*":
+			case "/":
+				while (!stk.isEmpty() && (stk.peek().equals("*") || stk.peek().equals("/"))) {
+					r.add( stk.pop() );
 				}
-				stk.push(new Character(c));
+				stk.push(s);
 				break;
-			case '+':
-			case '-':
-				while (!stk.isEmpty() && ((Character) stk.peek()).charValue() != '(') {
-					postfixExpr.append(((Character) stk.pop()).charValue());
+			case "+":
+			case "-":
+				while (!stk.isEmpty() && !stk.peek().equals("(")) {
+					r.add( stk.pop() );
 				}
-				stk.push(new Character(c));
+				stk.push(s);
 				break;
-			case '(':
-				stk.push(new Character(c));
+			case "(":
+				stk.push(s);
 				break;
-			case ')':
-				while (!stk.isEmpty() && ((Character) stk.peek()).charValue() != '(') {
-					postfixExpr.append(((Character) stk.pop()).charValue());
+			case ")":
+				while (!stk.isEmpty() && !stk.peek().equals("(")) {
+					r.add(stk.pop());
 				}
-				if (((Character) stk.peek()).charValue() == '(')
+				if (stk.peek().equals("("))
 					stk.pop();
 				break;
 			default:
-				postfixExpr.append(c);
+				r.add(s);
 			}
 		}
 
 		while (stk != null && !stk.isEmpty()) {
-			postfixExpr.append(((Character) stk.pop()).charValue());
+			r.add( stk.pop() );
 		}
 
-		return postfixExpr.toString();
+		return r;
 	}	
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
+//		String s = "1 + 1";
+//		String s = " 2-1 + 2 ";
 //		String s = "3+2*2";
 //		String s = " 3/2 ";
 //		String s = " 3+5 / 2 ";
 		String s = "1*2-3/4+5*6-7*8+9/10";
-		parse3(s);
+//		String s = "(1+(4+5+2)-3)+(6+8)";
+//		parse3(s);
 		System.out.println( s + " = " + calculate(s) );
 	}
 	
