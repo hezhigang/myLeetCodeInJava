6d08b6ee1b2e608a79d3c68969848b794f4d87c4
hezhigang <hezhigangthink@gmail.com>
2020-03-17 13:55:39 -0400

    509. Fibonacci Number

--- src/com/leetcode/FibonacciNumber.java
+++ src/com/leetcode/FibonacciNumber.java
@@ -28,7 +28,7 @@ public class FibonacciNumber {
      * @param N
      * @return
      */
-    public static int fib_dyn(int N) {
+    public static int fib_dp(int N) {
 		int r=0, recip1st=0, recip2nd=1;
 		if (N == 0)
 			r = 0;
@@ -41,13 +41,24 @@ public class FibonacciNumber {
 		}
 		return r;
     }
+    
+    /**
+     * Binet's formula
+     * @param N
+     * @return
+     */
+    public static int fib_formula(int N) {
+    	final double SQRT5 = Math.sqrt(5);
+    	Double d = ( Math.pow((1+SQRT5)/2,N)-Math.pow((1-SQRT5)/2,N) )/SQRT5;
+    	return d.intValue();
+    }
 
 	public static void main(String[] args) {
-		System.out.printf("F(%d)=%d", 2, fib(2));
+		System.out.printf("F(%d)=%d", 2, fib_formula(2));
 		System.out.println();
-		System.out.printf("F(%d)=%d", 3, fib(3));
+		System.out.printf("F(%d)=%d", 3, fib_formula(3));
 		System.out.println();
-		System.out.printf("F(%d)=%d", 4, fib(4));
+		System.out.printf("F(%d)=%d", 4, fib_formula(4));
 	}
 
 }
