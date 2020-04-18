9842675c7e2e41f2f29b1907167878279805fb83
hezhigang <hezhigangthink@gmail.com>
2020-04-18 01:34:06 -0400

    867. Transpose Matrix

---
+++ src/com/leetcode/TransposeMatrix.java
@@ -0,0 +1,30 @@
+package com.leetcode;
+
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+/**
+ * 867. Transpose Matrix
+ * https://leetcode.com/problems/transpose-matrix
+ */
+public class TransposeMatrix {
+
+    public static int[][] transpose(int[][] A) {
+        int h = A.length;
+        int w = A[0].length;
+        int[][] B = new int[w][h];
+        for(int i=0; i<h; i++) {
+            for(int j=0; j<w; j++)
+                B[j][i]=A[i][j];
+        }
+        return B;
+    }
+
+    public static void main(String[] args) {
+//        int[][] A = {{1, 2, 3}, {4, 5, 6}};
+        int[][] A = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        Stream.of(A).map(Arrays::toString).forEach(System.out::println);
+        int[][] B = transpose(A);
+        Stream.of(B).map(Arrays::toString).forEach(System.out::println);
+    }
+}
