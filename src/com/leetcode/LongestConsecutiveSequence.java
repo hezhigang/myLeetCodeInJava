e8cead0ccaaf75a4d61d0d675d855b50676eca8d
hezhigang <hezhigangthink@gmail.com>
2020-05-06 00:02:52 -0400

    128. Longest Consecutive Sequence, Accepted by leetcode

--- src/com/leetcode/LongestConsecutiveSequence.java
+++ src/com/leetcode/LongestConsecutiveSequence.java
@@ -8,6 +8,21 @@ import java.util.*;
  */
 public class LongestConsecutiveSequence {
 
+    private static int[] removeDuplicates(int[] input) {
+        if (input == null || input.length <= 0) {
+            return input;
+        }
+        Set<Integer> aSet = new HashSet<>(input.length);
+        for (int i : input) {
+            aSet.add(i);
+        }
+        int[] a = new int[aSet.size()];
+        int i=0;
+        for(Integer x : aSet)
+            a[i++] = x;
+        return a;
+    }
+
     /**
      *
      * Your algorithm should run in O(n) complexity.
@@ -15,27 +30,48 @@ public class LongestConsecutiveSequence {
      * @return
      */
     public static int longestConsecutive(int[] nums) {
-        int N = nums.length;
+        int[] arr = removeDuplicates(nums);
+        System.out.printf("remove duplicate elements array as : %s", Arrays.toString(arr) );
+        System.out.println();
+        int N = arr.length;
         Map<Integer, Integer> map = new HashMap<Integer, Integer>();
         for(int i=0; i<N; i++) {
-            map.put(nums[i]-1, i);
-            map.put(nums[i]+1, i);
+            if (!map.containsKey(arr[i]-1)) map.put(arr[i]-1, i);
+            if (!map.containsKey(arr[i]+1)) map.put(arr[i]+1, i);
         }
         int len = 0;
         UF uf = new UF(N);
         for(int i=0; i<N; i++) {
-            if (map.containsKey(nums[i])) {
-                uf.union(i, map.get(nums[i]));
-
+            if (map.containsKey(arr[i])) {
+                uf.union(i, map.get(arr[i]));
             }
         }
         System.out.printf("count of components = %d \n", uf.count());
+        int[] parent = uf.parent();
+        System.out.printf("parent index as : %s", Arrays.toString(parent) );
+        System.out.println();
+        Map<Integer,Integer> tmpMap = new HashMap<Integer, Integer>();
+        int k;
+        for(int i=0; i<parent.length; i++) {
+            k = parent[i];
+            if (tmpMap.containsKey(parent[i]))
+                tmpMap.put(k, tmpMap.get(k)+1);
+            else
+                tmpMap.put(k, 1);
+        }
+        for(Integer n : tmpMap.values()) {
+            if (n>len)
+                len = n;
+        }
+
         return len;
     }
 
     public static void main(String[] args) {
 //        int[] nums = {100, 4, 200, 1, 3, 2};
-        int[] nums = {4, 2, 3, 11, 12, 9, 10, 900};
+//        int[] nums = {4, 2, 3, 11, 12, 9, 10, 900};
+//        int[] nums = {0, 0, -1};
+        int[] nums = {1, -8, 7, -2, -4, -4, 6, 3, -4, 0, -7, -1, 5, 1, -9, -3};
         System.out.printf("unsorted array as : %s", Arrays.toString(nums) );
         System.out.println();
         int len = longestConsecutive(nums);
