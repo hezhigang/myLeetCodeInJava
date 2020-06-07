49f77123a5daf76385632310b84d4bb4577269e7
hezhigang <hezhigangthink@gmail.com>
2020-06-07 00:50:18 -0400

    172. Factorial Trailing Zeroes

--- src/com/leetcode/FactorialTrailingZeroes.java
+++ src/com/leetcode/FactorialTrailingZeroes.java
@@ -9,6 +9,17 @@ public class FactorialTrailingZeroes {
     public static int trailingZeroes(int n) {
         if (n < 5)
             return 0;
+        int x = 0;
+        for (long p = 5; n >= p; ) {
+            x += n / p;
+            p *= 5;
+        }
+        return x;
+    }
+
+    public static int trailingZeroes_v3(int n) {
+        if (n < 5)
+            return 0;
         long x = 0;
         long p = 5;
         while (n >= p) {
@@ -70,9 +81,11 @@ public class FactorialTrailingZeroes {
         int c = trailingZeroes(n);
         int c1 = trailingZeroes_v1(n);
         int c2 = trailingZeroes_v2(n);
+        int c3 = trailingZeroes_v3(n);
 
-        System.out.printf("the number of trailing zeroes in %d! = %d \n", n, c);
-        System.out.printf("the number of trailing zeroes in %d! = %d \n", n, c1);
-        System.out.printf("the number of trailing zeroes in %d! = %d \n", n, c2);
+        System.out.printf("the number of trailing zeroes in %d! = %d\n", n, c);
+        System.out.printf("the number of trailing zeroes in %d! = %d\n", n, c1);
+        System.out.printf("the number of trailing zeroes in %d! = %d\n", n, c2);
+        System.out.printf("the number of trailing zeroes in %d! = %d\n", n, c3);
     }
 }
