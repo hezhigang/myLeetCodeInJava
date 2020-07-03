1ee54f81df26ca481806d60b1fbf5bc620b21f24
hezhigang <hezhigangthink@gmail.com>
2020-07-03 17:33:02 -0400

    Definition for singly-linked list

--- src/com/leetcode/ListNode.java
+++ src/com/leetcode/ListNode.java
@@ -10,9 +10,18 @@ package com.leetcode;
  */
 public class ListNode {
 	int val;
+
 	ListNode next;
 
-	ListNode(int x) {
-		val = x;
+	ListNode() {
+	}
+
+	ListNode(int val) {
+		this.val = val;
+	}
+
+	ListNode(int val, ListNode next) {
+		this.val = val;
+		this.next = next;
 	}
 }
