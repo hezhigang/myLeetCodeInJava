a64a32b522b6a76e542eaa84fc41d9d3af565196
hezhigang <hezhigangthink@gmail.com>
2020-07-03 17:33:31 -0400

    21. Merge Two Sorted Lists

--- src/com/leetcode/MergeTwoSortedLists.java
+++ src/com/leetcode/MergeTwoSortedLists.java
@@ -9,6 +9,41 @@ package com.leetcode;
  * 21. Merge Two Sorted Lists
  */
 public class MergeTwoSortedLists {
+
+	/**
+	 * defining a fake head
+	 * https://www.programcreek.com/2012/12/leetcode-merge-two-sorted-lists-java/
+	 * @param l1
+	 * @param l2
+	 * @return
+	 */
+	public static ListNode mergeTwoLists2(ListNode l1, ListNode l2) {
+		ListNode head = new ListNode(0);
+		ListNode p = head;
+
+		ListNode p1 = l1;
+		ListNode p2 = l2;
+		while (p1 != null && p2 != null) {
+			if (p1.val < p2.val) {
+				p.next = p1;
+				p1 = p1.next;
+			} else {
+				p.next = p2;
+				p2 = p2.next;
+			}
+			p = p.next;
+		}
+
+		if (p1 != null) {
+			p.next = p1;
+		}
+
+		if (p2 != null) {
+			p.next = p2;
+		}
+
+		return head.next;
+	}
 	
 	/**
 	 * Java, 1 ms, 4 lines codes, using recursion
@@ -46,7 +81,8 @@ public class MergeTwoSortedLists {
 		ListNode list2_node3 = new ListNode(4);
 		list2_node2.next = list2_node3;
 		
-		ListNode list = mergeTwoLists(list1_node1,list2_node1);
+//		ListNode list = mergeTwoLists(list1_node1,list2_node1);
+		ListNode list = mergeTwoLists2(list1_node1,list2_node1);
 		
 		t(list);
 	}
@@ -60,6 +96,6 @@ public class MergeTwoSortedLists {
 			}			
 			node = node.next;
 		}
-	}	
+	}
 
 }
