b8624ac61aca30371ffbf3cec5dcf25efbebc516
hezhigang <hezhigangthink@gmail.com>
2020-07-11 12:14:20 -0400

    231. Power of Two

--- src/com/leetcode/PowerofTwo.java
+++ src/com/leetcode/PowerofTwo.java
@@ -24,6 +24,18 @@ public class PowerofTwo {
         return logbase2 - logbase2.intValue() <= 0.00000000000001;
     }
 
+    /**
+     * bit magic
+     * https://leetcode.com/problems/power-of-two/discuss/699658/java-bit-magic
+     * @param n
+     * @return
+     */
+    public static boolean isPowerOfTwo_bitmagic(int n) {
+        if (n < 1)
+            return false;
+        return (n & (n - 1)) == 0;
+    }
+
     public static void main(String[] args) {
 //        int n = 1;
 //        int n = 16;
@@ -31,12 +43,15 @@ public class PowerofTwo {
 //        int n = 3;
 //        int n = 4;
 //        int n = 2;
-//        int n = 536870912;
-        int n = 0;
+        int n = 536870912;
+//        int n = 0;
         boolean b = isPowerOfTwo(n);
         System.out.printf("%d %s a power of two\n\r", n, b?"is":"is not");
         System.out.println("---------------");
         boolean b2 = isPowerOfTwo_logrithm(n);
         System.out.printf("%d %s a power of two\n\r", n, b2?"is":"is not");
+        System.out.println("---------------");
+        boolean b3 = isPowerOfTwo_bitmagic(n);
+        System.out.printf("%d %s a power of two\n\r", n, b3?"is":"is not");
     }
 }
