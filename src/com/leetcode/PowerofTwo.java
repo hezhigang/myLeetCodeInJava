21a47a61cd5dd768fe0751080e3f7d9992d049e3
hezhigang <hezhigangthink@gmail.com>
2020-07-13 21:34:42 -0400

    231. Power of Two

--- src/com/leetcode/PowerofTwo.java
+++ src/com/leetcode/PowerofTwo.java
@@ -36,6 +36,10 @@ public class PowerofTwo {
         return (n & (n - 1)) == 0;
     }
 
+    public static boolean isPowerOfTwo_limit(int n) {
+        return n > 0 && 1073741824 % n == 0;
+    }
+
     public static void main(String[] args) {
 //        int n = 1;
 //        int n = 16;
@@ -53,5 +57,10 @@ public class PowerofTwo {
         System.out.println("---------------");
         boolean b3 = isPowerOfTwo_bitmagic(n);
         System.out.printf("%d %s a power of two\n\r", n, b3?"is":"is not");
+        int max2power = new Double(Math.pow(2,Math.floor(Math.log(Integer.MAX_VALUE)/Math.log(2)))).intValue();
+        System.out.printf("nax 2 power integer=%d\n\r",max2power);
+        System.out.println("---------------");
+        boolean b4 = isPowerOfTwo_limit(n);
+        System.out.printf("%d %s a power of two\n\r", n, b4?"is":"is not");
     }
 }
