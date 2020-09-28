ce20d2661c6ad888526e5eeac49987383155e315
hezhigang <hezhigangthink@gmail.com>
2020-09-28 11:51:26 -0400

    1. Two Sum

--- src/com/leetcode/TwoSum.java
+++ src/com/leetcode/TwoSum.java
@@ -1,21 +1,22 @@
 package com.leetcode;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 1. Two Sum
+ * https://leetcode.com/problems/two-sum/
+ */
 public class TwoSum {
+
 	/**
-	 * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
-
-You may assume that each input would have exactly one solution, and you may not use the same element twice.
-
-Example:
-
-Given nums = [2, 7, 11, 15], target = 9,
-
-Because nums[0] + nums[1] = 2 + 7 = 9,
-return [0, 1].
-	 * @param args
+	 * brute method
+	 * @param nums
+	 * @param target
+	 * @return
 	 */
-	
-    public static int[] twoSum(int[] nums, int target) {
+    public static int[] twoSum_brute(int[] nums, int target) {
         int[] idxArr = {0,0};
         for(int i=0;i<nums.length;i++) 
         	for(int j=nums.length-1;j>i;j--) {
@@ -25,17 +26,40 @@ return [0, 1].
         			return idxArr;
         		}
         	}
-        
         return idxArr;
     }
+
+	/**
+	 * Approach 3: One-pass Hash Table
+	 * https://leetcode.com/problems/two-sum/solution/
+	 * Sep 28, 2020
+	 * @param nums
+	 * @param target
+	 * @return
+	 */
+	public static int[] twoSum(int[] nums, int target) {
+		int a = 0, b = 0;
+		Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+		for (int i = 0; i < nums.length; i++) {
+			int complement = target - nums[i];
+			if (map.containsKey(complement)) {
+				a = i;
+				b = map.get(complement);
+			} else {
+				map.put(nums[i], i);
+			}
+		}
+		return new int[]{a, b};
+	}
 
 	public static void main(String[] args) {
 		int[] nums = {1, 7, 3, 6, 20, 15};
 		int target = 10;
 		int[] idxArr = twoSum(nums,target);
-		for (int i=0;i<idxArr.length;i++) {
-			System.out.println(idxArr[i]);
-		}
+//		for (int i=0;i<idxArr.length;i++) {
+//			System.out.println(idxArr[i]);
+//		}
+		System.out.printf("nums=%s, target=%d, Output: %s", Arrays.toString(nums), target, Arrays.toString(idxArr));
 	}
 
 }
