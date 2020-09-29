714048304b7813fd90e198ea877bbdbb2d6c56ae
hezhigang <hezhigangthink@gmail.com>
2020-09-29 14:49:26 -0400

    nothing

---
+++ src/com/leetcode/Pancakesorting.java
@@ -0,0 +1,28 @@
+/**
+ * 969. Pancake Sorting
+ * https://leetcode.com/problems/pancake-sorting/
+ */
+package com.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author hezhigang
+ *
+ */
+public class Pancakesorting {
+	
+    public List<Integer> pancakeSort(int[] A) {
+    	List<Integer> list = new ArrayList<Integer>();
+    	
+    	return list;
+    }
+
+
+	public static void main(String[] args) {
+		int[] a1 = {3,2,4,1};
+		int[] a2 = {1,2,3};
+	}
+
+}
