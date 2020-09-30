061ce0ff999cf8be189d5bf5850316d3a52ae650
hezhigang <hezhigangthink@gmail.com>
2020-09-29 22:57:25 -0400

    15. 3Sum

--- src/com/leetcode/ThreeSum.java
+++ src/com/leetcode/ThreeSum.java
@@ -150,7 +150,7 @@ public class ThreeSum {
      * @param nums
      * @return
      */
-    public static List<List<Integer>> threeSum(int[] nums) {
+    public static List<List<Integer>> threeSum_wikipedia(int[] nums) {
         final int N = nums.length;
         Arrays.sort(nums);
         Set<List<Integer>> set = new HashSet<List<Integer>>();
@@ -175,6 +175,52 @@ public class ThreeSum {
             }
         }
         return new ArrayList<>(set);
+    }
+
+    /**
+     * two pointers
+     * https://www.programcreek.com/2012/12/leetcode-3sum/
+     * Runtime: 21 ms, faster than 65.13% of Java online submissions for 3Sum.
+     * Memory Usage: 43.1 MB, less than 88.54% of Java online submissions for 3Sum.
+     * @param nums
+     * @return
+     */
+    public static List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+
+        ArrayList<List<Integer>> result = new ArrayList<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int j = i + 1;
+            int k = nums.length - 1;
+
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            while (j < k) {
+                if (k < nums.length - 1 && nums[k] == nums[k + 1]) {
+                    k--;
+                    continue;
+                }
+
+                if (nums[i] + nums[j] + nums[k] > 0) {
+                    k--;
+                } else if (nums[i] + nums[j] + nums[k] < 0) {
+                    j++;
+                } else {
+                    ArrayList<Integer> l = new ArrayList<>();
+                    l.add(nums[i]);
+                    l.add(nums[j]);
+                    l.add(nums[k]);
+                    result.add(l);
+                    j++;
+                    k--;
+                }
+            }
+        }
+
+        return result;
     }
 
     public static void main(String[] args) {
