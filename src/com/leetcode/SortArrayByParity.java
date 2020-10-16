c3b073086d70487cd7e98405c044151baeba27dc
hezhigang <hezhigangthink@gmail.com>
2020-10-16 14:31:32 -0400

    905. Sort Array By Parity, In-Place

--- src/com/leetcode/SortArrayByParity.java
+++ src/com/leetcode/SortArrayByParity.java
@@ -13,13 +13,14 @@ import java.util.Arrays;
  */
 public class SortArrayByParity {
     /**
+     * Two Pass
      * 285 / 285 test cases passed.
      * Runtime: 1 ms, faster than 99.47% of Java online submissions for Sort Array By Parity.
      * Memory Usage: 40.1 MB, less than 30.13% of Java online submissions for Sort Array By Parity.
      * @param A
      * @return
      */
-    public static int[] sortArrayByParity(int[] A) {
+    public static int[] sortArrayByParity_twopass(int[] A) {
         final int N = A.length;
         int[] B = new int[N];
         int left = 0, right = N - 1;
@@ -33,9 +34,34 @@ public class SortArrayByParity {
         return B;
     }
 
+    /**
+     * In-Place
+     * https://leetcode.com/problems/sort-array-by-parity/solution/
+     * @param A
+     * @return
+     */
+    public static int[] sortArrayByParity(int[] A) {
+        final int N = A.length;
+        int left = 0, right = N - 1;
+        int temp;
+        while (left < right) {
+            if ((A[left] & 1) == 1 && (A[right] & 1) == 0) {
+                temp = A[left];
+                A[left] = A[right];
+                A[right] = temp;
+            }
+            if ((A[left] & 1) == 0) left++;
+            if ((A[right] & 1) == 1) right--;
+        }
+        return A;
+    }
+
     public static void main(String[] args) {
         int[] A = {3, 1, 2, 4};
-        int[] B = sortArrayByParity(A);
-        System.out.printf("Input:: %s, \n\rOutput: %s", Arrays.toString(A), Arrays.toString(B));
+        System.out.printf("Input: %s\n\r", Arrays.toString(A));
+        sortArrayByParity(A);
+        System.out.printf("Output: %s", Arrays.toString(A));
+//        int[] B = sortArrayByParity(A);
+//        System.out.printf("Input: %s, \n\rOutput: %s", Arrays.toString(A), Arrays.toString(B));
     }
 }
