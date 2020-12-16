dfe122975aaa810d70540fff63ecec7b61e01816
hezhigang <hezhigangthink@gmail.com>
2020-12-15 22:08:41 -0500

    709. To Lower Case

--- src/com/leetcode/LowerCase.java
+++ src/com/leetcode/LowerCase.java
@@ -21,7 +21,7 @@ public class LowerCase {
 	 * @param str
 	 * @return
 	 */
-    public static String toLowerCase(String str) {
+    public static String toLowerCase_java(String str) {
     	String s = "";
     	for(int i=0;i<str.length();i++) {
     		s += Character.toLowerCase(str.charAt(i));
@@ -29,9 +29,27 @@ public class LowerCase {
     	return s;
     }
 
+	/**
+	 * 10 cool bitwise operator hacks and tricks every programmer must know
+	 * Quickly convert character to lowercase and uppercase
+	 * https://codeforwin.org/2018/05/10-cool-bitwise-operator-hacks-and-tricks.html
+	 * 8 / 8 test cases passed.
+	 * Runtime: 0 ms, faster than 100.00% of Java online submissions for To Lower Case.
+	 * Memory Usage: 36.9 MB, less than 46.27% of Java online submissions for To Lower Case.
+	 * @param str
+	 * @return
+	 */
+	public static String toLowerCase(String str) {
+		char[] a = str.toCharArray();
+		for (int i = 0; i < a.length; i++) {
+			a[i] = (char) (a[i] | ' ');
+		}
+		return String.valueOf(a);
+	}
+
 	public static void main(String[] args) {
-//		String s = "Hello";
-		String s = "LOVELY";
+		String s = "Hello";
+//		String s = "LOVELY";
 		System.out.printf("Lower Case of %s: %s, %s", s, s.toLowerCase(), toLowerCase(s));
 		System.out.println();
 	}
