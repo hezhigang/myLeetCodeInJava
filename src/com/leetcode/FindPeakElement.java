fb1b0fcfacb2f4fab3b429510536b760c293143e
Zhigang He <hezhigang1024@gmail.com>
2022-03-11 23:06:42 -0500

    162. Find Peak Element

--- src/com/leetcode/FindPeakElement.java
+++ src/com/leetcode/FindPeakElement.java
@@ -50,13 +50,52 @@ public class FindPeakElement {
         return left;
     }
 
+    /**
+     * Divide and Conquer Approach
+     * Find Peak Element in an Array
+     * https://iq.opengenus.org/peak-element-in-array/
+     * @param nums
+     * @param left
+     * @param right
+     * @return
+     */
+    private static int recur(int[] nums, int left, int right) {
+        if (left == right)
+            return left;
+        else if (left + 1 == right) {
+            if (nums[left] > nums[right])
+                return left;
+            else
+                return right;
+        } else {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1])
+                return mid;
+            else if (nums[mid] < nums[mid - 1] && nums[mid] > nums[mid + 1])
+                return recur(nums, left, mid - 1);
+            else
+                return recur(nums, mid + 1, right);
+        }
+    }
+
+    /**
+     * 63 / 63 test cases passed.
+     * Runtime: 0 ms, faster than 100.00% of Java online submissions for Find Peak Element.
+     * Memory Usage: 42.5 MB, less than 49.41% of Java online submissions for Find Peak Element.
+     * @param nums
+     * @return
+     */
+    public static int findPeakElement_recur(int[] nums) {
+        return recur(nums, 0, nums.length - 1);
+    }
+
     public static void main(String[] args) {
 //        int[] nums = {1, 2, 3, 1};
-//        int[] nums = {1, 2, 1, 3, 5, 6, 4};
+        int[] nums = {1, 2, 1, 3, 5, 6, 4};
 //        int[] nums = {1, 2};
 //        int[] nums = {1};
 //        int[] nums = {140, 150, 5, 2, 8, 80, 122, 180};
-        int[] nums = {6, 5, 4, 3, 2, 3, 2};
+//        int[] nums = {6, 5, 4, 3, 2, 3, 2};
 //        int[] nums = {8, 7, 6, 5, 4};
         int index = findPeakElement(nums);
         System.out.printf("one of peak elements in %s is %d", Arrays.toString(nums), nums[index]);
