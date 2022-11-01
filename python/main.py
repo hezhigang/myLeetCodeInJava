a019097ad8244fde35a325c3d7930924cf5f1a88
Zhigang He <hezhigang1024@gmail.com>
2022-10-31 21:20:01 -0400

    main.py

--- python/main.py
+++ python/main.py
@@ -1,14 +1,5 @@
-from objects.MaxConsecutiveOnes import MaxConsecutiveOnes
-
-
 def main():
-
-    # 485. Max Consecutive Ones
-    nums = [1,1,0,1,1,1]
-    # nums = [1,0,1,1,0,1]
-    o = MaxConsecutiveOnes()
-    i = o.findMaxConsecutiveOnes(nums)
-    print( "The maximum number of consecutive 1s in [{0:s}] = {1:d}".format(','.join(str(x) for x in nums), i) )
+    print("Hello, World!")
 
 if __name__ == '__main__':
     main()
