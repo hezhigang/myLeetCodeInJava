95fee482460160f2863b7723c2baf166a81f37cb
Zhigang He <hezhigang1024@gmail.com>
2023-08-16 11:40:31 -0400

    147. Insertion Sort List

--- src/com/leetcode/InsertionSortList.java
+++ src/com/leetcode/InsertionSortList.java
@@ -12,25 +12,78 @@ package com.leetcode;
 public class InsertionSortList {
 
     /**
-     *
+     * https://www.enjoyalgorithms.com/blog/sort-linked-list-using-insertion-sort
+     * Accepted
+     * Runtime 18ms Beats 63.20% of users with Java
+     * Memory 42.99mb Beats 90.09% of users with Java
      * @param head
      * @return
      */
     public static ListNode insertionSortList(ListNode head) {
-        for (ListNode node = head; node != null; node = node.next) {
-
+        ListNode node = head;
+        ListNode sortedHead = null;
+        while (node != null) {
+            ListNode nextNode = node.next;
+            sortedHead = sortedInsert(sortedHead, node);
+            node = nextNode;
         }
-        return head;
+        return sortedHead;
+    }
+
+    /**
+     * https://www.enjoyalgorithms.com/blog/sort-linked-list-using-insertion-sort
+     * @param sortedHead
+     * @param node
+     * @return
+     */
+    private static ListNode sortedInsert(ListNode sortedHead, ListNode node) {
+        if (sortedHead == null || sortedHead.val >= node.val) {
+            node.next = sortedHead;
+            return node;
+        } else {
+            ListNode temp = sortedHead;
+            while (temp.next != null && temp.next.val < node.val) {
+                temp = temp.next;
+            }
+            node.next = temp.next;
+            temp.next = node;
+        }
+        return sortedHead;
     }
 
     public static void main(String[] args) {
-        ListNode head = new ListNode(4);
-        ListNode list_node2 = new ListNode(2);
+        ListNode head = new ListNode(6);
+        ListNode list_node2 = new ListNode(5);
         head.next = list_node2;
-        ListNode list_node3 = new ListNode(1);
+        ListNode list_node3 = new ListNode(3);
         list_node2.next = list_node3;
-        ListNode list_node4 = new ListNode(3);
+        ListNode list_node4 = new ListNode(1);
         list_node3.next = list_node4;
+        ListNode list_node5 = new ListNode(8);
+        list_node4.next = list_node5;
+        ListNode list_node6 = new ListNode(7);
+        list_node5.next = list_node6;
+        ListNode list_node7 = new ListNode(2);
+        list_node6.next = list_node7;
+        ListNode list_node8 = new ListNode(4);
+        list_node7.next = list_node8;
+
+//        head = insertionSortList(head);
+
+//        ListNode head = new ListNode(4);
+//        ListNode list_node2 = new ListNode(2);
+//        head.next = list_node2;
+//        ListNode list_node3 = new ListNode(1);
+//        list_node2.next = list_node3;
+//        ListNode list_node4 = new ListNode(3);
+//        list_node3.next = list_node4;
+
+        t(head);
+
+        System.out.println();
+
+        head = insertionSortList(head);
+
         t(head);
     }
 
