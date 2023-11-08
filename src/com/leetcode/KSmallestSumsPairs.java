fcaf1d9748797b3320c7263479cc568607d323c3
Zhigang He <hezhigang1024@gmail.com>
2023-11-08 13:41:18 -0500

    373. Find K Pairs with Smallest Sums, Accepted

--- src/com/leetcode/KSmallestSumsPairs.java
+++ src/com/leetcode/KSmallestSumsPairs.java
@@ -9,80 +9,55 @@ import java.util.stream.Collectors;
  * Oct 24, 2023
  */
 public class KSmallestSumsPairs {
-
+    /**
+     * Accepted
+     * Runtime 83ms Beats 14.24% of users with Java
+     * Memory 56.03MB Beats 93.45% of users with Java
+     * @param nums1
+     * @param nums2
+     * @param k
+     * @return
+     */
     public static List<List<Integer>> kSmallestPairs(int[] nums1, int[] nums2, int k) {
+        int len1 = nums1.length;
+        int len2 = nums2.length;
         List<List<Integer>> list = new ArrayList<List<Integer>>();
-        PriorityQueue<Node> pq = new PriorityQueue<Node>(k);
-        for (int i = 0; i < nums1.length; i++) {
-            for (int j = 0; j < nums2.length; j++) {
-                Node node = new Node();
-                node.setSumUp(nums1[i] + nums2[j]);
-                node.setElements(Arrays.asList(nums1[i], nums2[j]));
-                pq.add(node);
-                if (pq.size() > k)
-                    pq.poll();
+        Set<String> visited = new HashSet<String>();
+        PriorityQueue<int[]> minHeap = new PriorityQueue<>((a, b) -> a[0] - b[0]);
+        minHeap.offer(new int[]{nums1[0] + nums2[0], 0, 0});
+        visited.add("0,0");
+        String key = "";
+        while (k-- > 0 && !minHeap.isEmpty()) {
+            int[] top = minHeap.poll();
+            int i = top[1];
+            int j = top[2];
+
+            list.add(Arrays.asList(nums1[i], nums2[j]));
+            key = Integer.toString(i + 1) + "," + j;
+            if (i + 1 < len1 && !visited.contains(key)) {
+                minHeap.offer(new int[]{nums1[i + 1] + nums2[j], i + 1, j});
+                visited.add(key);
+            }
+
+            key = i + "," + Integer.toString(j + 1);
+            if (j + 1 < len2 && !visited.contains(key)) {
+                minHeap.offer(new int[]{nums1[i] + nums2[j + 1], i, j + 1});
+                visited.add(key);
             }
         }
-        Node node = null;
-        while (list.size() < k) {
-            node = pq.poll();
-            if (node != null)
-                list.add(node.getElements());
-        }
-        List<List<Integer>> rlist = new ArrayList<List<Integer>>();
-        for (int i = list.size() - 1; i >= 0; i--) {
-            rlist.add(list.get(i));
-        }
-        return rlist;
+        return list;
     }
 
     public static void main(String[] args) {
-//        int[] nums1 = {1, 7, 11};
-//        int[] nums2 = {2, 4, 6};
-//        int k = 3;
-
-        int[] nums1 = {1, 2, 3};
-        int[] nums2 = {2, 3, 4};
+        int[] nums1 = {1, 7, 11};
+        int[] nums2 = {2, 4, 6};
         int k = 3;
+
+//        int[] nums1 = {1, 2, 3};
+//        int[] nums2 = {2, 3, 4};
+//        int k = 3;
 
         List<List<Integer>> list = kSmallestPairs(nums1, nums2, k);
         System.out.println(list.stream().map(Object::toString).collect(Collectors.joining("\n")));
-    }
-}
-
-class Node implements Comparable<Node> {
-    private Integer sumUp;
-
-    private Integer getSumUp() {
-        return sumUp;
-    }
-
-    public Node() {
-    }
-
-    public void setSumUp(Integer sumUp) {
-        this.sumUp = sumUp;
-    }
-
-    public List<Integer> getElements() {
-        return elements;
-    }
-
-    public void setElements(List<Integer> elements) {
-        this.elements = elements;
-    }
-
-    public List<Integer> elements;
-
-    @Override
-    public int compareTo(Node that) {
-        int eval1 = this.sumUp;
-        int eval2 = that.sumUp;
-        if (eval1 < eval2)
-            return 1;
-        else if (eval1 > eval2)
-            return -1;
-        else
-            return 0;
     }
 }
