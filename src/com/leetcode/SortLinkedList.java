4e97072daa49239342b4f547f4353afd04b52f65
Zhigang He <hezhigang1024@gmail.com>
2024-01-24 10:09:26 -0500

    148. Sort List

--- src/com/leetcode/SortLinkedList.java
+++ src/com/leetcode/SortLinkedList.java
@@ -8,10 +8,30 @@ package com.leetcode;
 /**
  * 148. Sort List
  * https://leetcode.com/problems/sort-list/
+ * A version of bottom-up mergesort is the method of choice for sorting data organized
+ * in a linked list. Consider the list to be sorted sublists of size 1, then pass through to
+ * make sorted subarrays of size 2 linked together, then size 4, and so forth. This method
+ * rearranges the links to sort the list in place (without creating any new list nodes).
+ * Both the top-down and bottom-up approaches to implementing a divide-andconquer
+ * algorithm are intuitive.
  */
 public class SortLinkedList {
 
     public static ListNode sortList(ListNode head) {
+        final int len = getLength(head);
+//        System.out.printf("length of linked list = %d", len);
+        ListNode left = head;
+        ListNode right = null;
+        for (int sz = 1; sz < len; sz *= 2) {
+            left = head;
+            for (int lo = 0; lo < len - sz; lo += sz + sz) {
+                int mid = lo + sz - 1;
+                int hi = Math.min(lo + sz + sz - 1, len - 1);
+                right = left.next;
+                left = mergeTwoLists(left, right);
+            }
+        }
+        head = left;
         return head;
     }
 
@@ -24,32 +44,37 @@ public class SortLinkedList {
         return n;
     }
 
+    /**
+     * How To Merge Two Sorted Lists
+     * https://hackernoon.com/how-to-merge-two-sorted-lists
+     * @param l1
+     * @param l2
+     * @return
+     */
     private static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        ListNode head = new ListNode();
-        ListNode p = head;
-
-        ListNode p1 = l1;
-        ListNode p2 = l2;
-        while (p1 != null && p2 != null) {
-            if (p1.val < p2.val) {
-                p.next = p1;
-                p1 = p1.next;
+        ListNode dummy = new ListNode(0);
+        ListNode current = dummy;
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                current.next = l1;
+                l1 = l1.next;
             } else {
-                p.next = p2;
-                p2 = p2.next;
+                current.next = l2;
+                l2 = l2.next;
             }
-            p = p.next;
+
+            current = current.next;
         }
 
-        if (p1 != null) {
-            p.next = p1;
+        if (l1 != null) {
+            current.next = l1;
         }
 
-        if (p2 != null) {
-            p.next = p2;
+        if (l2 != null) {
+            current.next = l2;
         }
 
-        return head.next;
+        return dummy.next;
     }
 
     public static void main(String[] args) {
@@ -65,9 +90,9 @@ public class SortLinkedList {
         printLinkedList(head);
         System.out.println("len=" + getLength(head));
 
-//        System.out.println("sorted: ");
-//		ListNode head2 = sortList(head);
-//		printLinkedList(head2);
+        System.out.println("sorted: ");
+		ListNode head2 = sortList(head);
+		printLinkedList(head2);
     }
 
     private static void printLinkedList(ListNode node) {
